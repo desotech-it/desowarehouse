c08f4298bb9a932b0cd5a4e818b0f40efd9855e9
cri-gramegna05 <cristgramegna@gmail.com>
2024-05-13 18:20:34 +0200

    implement function to handle authorization based on role

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var PDFDocument = require('pdfkit');
 
 const utils = require('../utils');
 const FormData = require('form-data');
 
 router.use(express.urlencoded({ extended: true }));
+async function getMetadata(token, title){
+  const role = await utils.getUserRole(token);
+  if(role=="admin")
+    return { title: title, show_labels: false, show_orders: true, show_users: true};
+  else if(role=="warehouse")
+    return {title: title, show_labels: true, show_orders: false, show_users: false}
+  else
+    return {title: title, show_labels: false, show_orders: false, show_users: false}
+  }
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   const token = req.cookies['token'];
+  
   utils.userIsLoggedIn(token)
-    .then(isLoggedIn => {
+    .then(async (isLoggedIn) => {
       if (isLoggedIn)
-        res.render('index', { title: 'Home' });
+        res.render('index', await getMetadata(token, 'Home'));
       else
         res.redirect('/login');
     })
@@ -24,6 +35,44 @@ router.get('/', function (req, res, next) {
 router.get('/login', function (req, res, next) {
   res.render('login', { title: 'Sign In' });
 });
+
+/* GET all orders */
+router.get('/allOrders', async function (req, res) {
+  
+  const token = req.cookies['token'];
+  const auth = 'Bearer ' + token;
+  // 200 OK if token is valid and I have permissions
+  // 403 Forbidden if token is valid and I do not have permissions
+  // 500 Internal Server Error if something bad happened server side
+  let response = null;
+  let id = null;
+  //call to get user data
+  try{
+    response = await axios.get('auth/me', {headers: {'Authorization': auth}});
+    if (response.status === 401) {
+      utils.redirectToLogin(res);
+      return;
+    }
+    id=response.data['id'];
+  }catch(e){
+    res.render('error', { message: e.message, error: { status: response.status } });
+  }
+  try {
+    response = await axios.get('/orders', { headers: { 'Authorization': auth } });
+    if (response.status === 401) {
+      utils.redirectToLogin(res);
+      return;
+    } else if (response.status === 200) {
+      const metadata = await getMetadata(token, "Orders");
+      res.render('orders', Object.assign({}, metadata, {orders:response.data}));
+    } else {
+      res.render('error', { message: 'Something went wrong', error: {} });
+    }
+  } catch (e) {
+    res.render('error', { message: e.message, error: { status: response.status } });
+  }
+  // res.render('orders', { title: 'Orders' });
+})
 
 /* GET orders */
 router.get('/orders', async function (req, res) {
@@ -52,7 +101,8 @@ router.get('/orders', async function (req, res) {
       utils.redirectToLogin(res);
       return;
     } else if (response.status === 200) {
-      res.render('orders', { title: 'Orders', orders: response.data });
+      const metadata = await getMetadata(token, 'Orders');
+      res.render('orders', Object.assign({}, metadata, {orders: response.data }));
     } else {
       res.render('error', { message: 'Something went wrong', error: {} });
     }
@@ -63,8 +113,24 @@ router.get('/orders', async function (req, res) {
 })
 
 /* GET labels */
-router.get('/labels', function (req, res) {
-  res.render('labels', { title: 'Labels' });
+router.get('/labels', async function (req, res) {
+  const token = req.cookies['token'];
+  const auth = 'Bearer ' + token;
+  let response = null;
+  try {
+    response = await axios.get('/shipments', { headers: { 'Authorization': auth } });
+    if (response.status === 401) {
+      utils.redirectToLogin(res);
+      return;
+    } else if (response.status === 200) {
+      const metadata = await getMetadata(token, "Labels");
+      res.render('labels', Object.assign({}, metadata, {shipments: response.data}));
+    } else {
+      res.render('error', { message: 'Something went wrong', error: {} });
+    }
+  } catch (e) {
+    res.render('error', { message: e.message, error: { status: response.status } });
+  }
 })
 
 /* POST login page. */
@@ -80,4 +146,53 @@ router.post('/login', async function (req, res, next) {
   res.redirect('/');
 });
 
+router.patch('/modifyOrder', async function (req, res, next){
+  const status = req.query.status;
+  const id = req.query.id;
+  const token = req.cookies['token'];
+  const auth = 'Bearer ' + token;
+  try{
+    if(status=="SHIPPED")
+      await axios.post('/shipments', {id: id})
+    let response = await axios.patch('/orders/'+id+'?status='+status);
+    if (response.status==400)
+      res.render('error', { message: 'Something went wrong', error: {} });
+    res.json({"message": "Successful", "status": status})
+  }catch(e){
+    res.render('error', { message: 'Something went wrong', error: {} });
+  }
+});
 module.exports = router;
+
+router.get('/pdf', function (req, res, next){
+  const id = req.query.id;
+  const datetime = req.query.datetime;
+  const order_id = req.query.order_id;
+  var myDoc = new PDFDocument({bufferPages: true});
+
+let buffers = [];
+myDoc.on('data', buffers.push.bind(buffers));
+myDoc.on('end', () => {
+
+    let pdfData = Buffer.concat(buffers);
+    res.writeHead(200, {
+    'Content-Length': Buffer.byteLength(pdfData),
+    'Content-Type': 'application/pdf',
+    'Content-disposition': 'attachment;filename=DETAILS.pdf',})
+    .end(pdfData);
+
+});
+
+myDoc.font('Times-Roman')
+     .fontSize(26)
+     .text(`SHIPMENT DETAILS`);
+
+myDoc
+    .fontSize(15)
+    .text(`
+    SHIPMENT ID: ${id}\n\n
+    ORDER ID: ${order_id}\n\n
+    DATETIME: ${datetime}
+    `);
+myDoc.end();
+});
